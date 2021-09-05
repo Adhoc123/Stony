beb63a7c0a182e2be7052cee697a31bde3e2611d
Adhoc123 <39966247+Adhoc123@users.noreply.github.com>
2021-09-05 18:09:12 +0600

    Add files via upload

---
+++ WC.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stony
+{
+    public static class WC
+    {
+        public const string ImagePath = @"\images\product\";
+        public const string SessionCart = "ShoppingCartSession";
+        public const string AdminRole = "Admin";
+        public const string CustomerRole = "Customer";
+        public const string EmailAdmin = "emonhasan1010@gmail.com";
+    }
+}
